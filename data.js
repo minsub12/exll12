5076187f979a169212d03a8432f61b5280607bb9
minsub12 <jejuds987@naver.com>
2024-05-09 14:04:25 +0900

    commit to main

---
+++ data.js
@@ -0,0 +1,68 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kakaotalk_20240428_151444449",
+      "name": "KakaoTalk_20240428_151444449",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9382030163727606,
+          "pitch": 0.5807396525279884,
+          "rotation": 6.283185307179586,
+          "target": "0-kakaotalk_20240428_151444449"
+        },
+        {
+          "yaw": 0.5543839528345593,
+          "pitch": -0.08792417702461819,
+          "rotation": 0,
+          "target": "0-kakaotalk_20240428_151444449"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4002422379860686,
+          "pitch": 0.19451650943857857,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.8356717984029061,
+          "pitch": -0.31442692884362344,
+          "title": "2222",
+          "text": "Text3333"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
